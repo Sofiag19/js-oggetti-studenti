0ba6b505ce932694ee6c9452339e103cb3150365
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-30 14:01:27 +0100

    seconda richiesta

--- script.js
+++ script.js
@@ -6,24 +6,65 @@
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 // oggetto con nome, cognome ed età e stampare a schermo le proprietà
-// metodo 1
+// metodo 1 - creao l'oggetto con già le proprietà
 var studente = {
   "nome" : "Matteo",
   "cognome" : "Rossi",
-  "eta" : 30
+  "età" : 30
 };
 
 for (var dati in studente) {
   console.log(studente[dati]);
 }
 
-// metodo 2
+// metodo 2 - creo l'oggetto vuoto e lo riempio con le proprietà successivamente
 var studente1 = {};
 
 studente1.nome = "Lucia";
 studente1.cognome = "Magnini";
-studente1.eta = 24;
+studente1.età = 24;
 
 for (var dati1 in studente1) {
   console.log(studente1[dati1]);
+}
+
+// creare array di studenti e stampare per ognuno nome e cognome
+elencoStudenti = [];
+
+var alunno1 = {};
+var alunno2 = {};
+var alunno3 = {};
+var alunno4 = {};
+var alunno5 = {};
+
+elencoStudenti.push(alunno1);
+elencoStudenti.push(alunno2);
+elencoStudenti.push(alunno3);
+elencoStudenti.push(alunno4);
+elencoStudenti.push(alunno5);
+
+alunno1.nome = "Pietro";
+alunno1.cognome = "Lazzaro";
+alunno1.eta = "20";
+
+alunno2.nome = "Francesca";
+alunno2.cognome = "Merli";
+alunno2.eta = "21";
+
+alunno3.nome = "Vittoria";
+alunno3.cognome = "Usigni";
+alunno3.eta = "19";
+
+alunno4.nome = "Stefano";
+alunno4.cognome = "Visconti";
+alunno4.eta = "26";
+
+alunno5.nome = "Giorgio";
+alunno5.cognome = "Ugolinelli";
+alunno5.eta = "24";
+
+console.log(elencoStudenti);
+
+for (var i = 0; i < elencoStudenti.length; i++) {
+    console.log(elencoStudenti[i].nome, elencoStudenti[i].cognome);
 }
